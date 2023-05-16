1348e9790497a2e5eea21f3e158a0dad847493ff
Luis Mora <ljmora1@utpl.edu.ec>
2023-05-16 15:56:55 -0500

    Consulta001

--- ejemplos/run2.py
+++ ejemplos/run2.py
@@ -6,15 +6,18 @@ trabajador01 = Miclase02("Apple Inc", 8479.235)
 # Presentar objeto; usar el método __st__
 print(str(trabajador01))
 
-
 # objeto 02
+trabajador02 = Miclase02("Google", 7856.5156)
+# Presentar objeto; usar el método __st__
+print(str(trabajador02))
+
 # crear ingresando valores por teclado
 empresa = input("Ingrese el nombre de su empresa:\n")
 salario = float(input("Ingrese su salario mensual:\n"))
 
-trabajador02 = Miclase02(empresa, salario)
+trabajador03 = Miclase02(empresa, salario)
 
 # Presentar objeto; usar el método __st__
-print(str(trabajador02))
+print(str(trabajador03))
 
 
