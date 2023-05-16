e2e4e527b12642d7dbf7a133736293187c1e772f
Luis Mora <ljmora1@utpl.edu.ec>
2023-05-16 15:48:16 -0500

    Consulta001

--- ejemplos/mis_clases.py
+++ ejemplos/mis_clases.py
@@ -1,13 +1,56 @@
-"""
-
-"""
-
+from typing import Any
 # Crear dos clases en Python
 # Usted defina el nombre y los atributos
 # Puede usar las mismas clases usadas en Java en los ejemplos estudiados
 
 # clase 01
 
+class Miclase:
+    def __init__(self, nombre, apellido, edad):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad =  edad
+
+    def get_nombre(self):
+        return self.nombre
+
+    def get_apellido(self):
+        return self.apellido
+
+    def get_edad(self):
+        return self.edad
+
+    def set_nombre(self, n):
+        self.nombre = n
+
+    def set_apellido(self, n):
+        self.apellido = n
+
+    def set_edad(self, n):
+        self.edad = n     
 
 
+    def __str__(self):
+        return f"\nMi nombre es: {self.nombre} {self.apellido}\nTengo {self.edad} años..\n"      
+    
 # clase 02
+
+class Miclase02:
+    def __init__(self, empresa, salario):
+        self.empresa = empresa
+        self.salario = salario
+
+    def get_empresa(self):
+        return self.empresa
+
+    def get_salario(self):
+        return self.salario
+    
+    def set_empresa(self, a):
+        self.empresa = a
+
+    def set_salario(self, a):
+        self.salario = a  
+
+    def __str__(self):
+        return f"\nLa empresa para la que trabaja es {self.empresa}\nPosee un salario mensual de: {self.salario:.2f} dólares..\n"         
--- ejemplos/run.py
+++ ejemplos/run.py
@@ -1,18 +1,22 @@
-"""
-
-"""
+from mis_clases import Miclase, Miclase02
 # Crear dos objetos de la clase 01
 
 # objeto 01
-
 # crear
-
+object01 = Miclase("Luis", "Mora", 18)
 # Presentar objeto; usar el método __st__
-
+print(str(object01))
 # objeto 02
+object02 = Miclase("Jose", "Sanchez", 56)
+print(str(object02))
+
 
 # crear ingresando valores por teclado
+empresa = input("Cual es el nombre de su empresa?\n")
+salario = float(input("Cual es su salario mensual?\n"))
+
+persona = Miclase02(empresa, salario)
 
 # Presentar objeto; usar el método __st__
-
+print(str(persona))
 
